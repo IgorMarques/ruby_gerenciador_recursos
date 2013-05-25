1374e0c561f2609b81f2da275f8981e294392e62
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-05-25 15:47:00 -0300

    Adicionando construtor de user

--- user.rb
+++ user.rb
@@ -1,3 +1,10 @@
 class User
   att_acessor :name, :cpf, :sector, :function
+
+  def initialize(name, cpf, sector, function)
+    @name = name
+    @cpf = cpf
+    @sector = sector
+    @function = function
+  end
 end
